c9edd5fc4299f6d92f4dc2a50bd30353cee15464
Naill <2jetsetter@gmail.com>
2017-06-10 00:59:19 +0300

    Operators

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log(fullAge);
 */
 
 //Lecture: variables 2
+/*
 var name = 'Niall';
 var age = 25;
 
@@ -38,3 +39,24 @@ var lastName = prompt('What is the last name');
 console.log(lastName);
 
 alert(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.');
+*/
+
+
+//Operators
+var now = 2016;
+var birthYear = now - 25;
+
+birthYear = now - 26*2;
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
