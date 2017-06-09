54729927cc49be99ea7b7458c767759ce6eff9d8
Naill <2jetsetter@gmail.com>
2017-06-09 11:21:19 +0300

    Variable mutation and other basics

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Lecture: variables
+/*
 var name = 'Niall';
 console.log(name);
 
@@ -9,3 +11,30 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
+
+//Lecture: variables 2
+var name = 'Niall';
+var age = 25;
+
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'student';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'twenty five';
+job = 'diver';
+
+console.log(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years ' + job + '. Is he married? ' + isMarried + '.');
