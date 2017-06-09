65913d3a018842de47cd2a467b74b55e37fdc1d6
Naill <2jetsetter@gmail.com>
2017-06-09 11:03:47 +0300

    variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+var name = 'Niall';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 25;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
