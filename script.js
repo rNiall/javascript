972d81744d7699726ae90f0548f2a81968f5f82f
Naill <2jetsetter@gmail.com>
2017-06-10 17:55:50 +0300

    Hoisting

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2017 - year);
+}
 
 
+//retirement(1990);
+
+var retirement = function (year) {
+    console.log(65 - (2017 - year));
+};
 
 
+//variables
 
+console.log(age);
+var age = 23;
 
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
 
-
-
-
-
-
-
-
-
+foo();
+console.log(age);
 
 ///////////////////////////////////////
 // Lecture: Scoping
