8c4583d20e5ee8c052db0473aa96413f38a71e94
Naill <2jetsetter@gmail.com>
2017-06-10 10:32:58 +0300

    Easy challenge completed

--- script.js
+++ script.js
@@ -95,34 +95,79 @@ if (23 === "23") {
 
 //boolean logic and switch
 
-var age = 20;
+// var age = 20;
+//
+// if (age < 20) {
+//     console.log('John is a teenager');
+// } else if (age >= 20 && age < 30) {
+//     console.log('John is a young man');
+// }
+// else {
+//     console.log('John is a man');
+// }
+//
+// var job = 'student';
+//
+// job =  prompt('What does John do');
+//
+// switch (job) {
+//     case 'student':
+//         console.log('John studies JS');
+//         break;
+//     case 'driver':
+//         console.log('John drives a cab in Lisbon');
+//         break;
+//     case 'cop':
+//         console.log('John helps fight crime');
+//         break;
+//     default:
+//         console.log('John does something else');
+// }
 
-if (age < 20) {
-    console.log('John is a teenager');
-} else if (age >= 20 && age < 30) {
-    console.log('John is a young man');
+
+
+var  ageJohn = 20;
+var ageMark = 18;
+var ageNiall = 25;
+var heightJohn = 175;
+var heightMark = 170;
+var heightNiall = 176;
+var scoreJohn = heightJohn + ageJohn * 5;
+var scoreMark = heightMark + ageMark * 5;
+var scoreNiall = heightNiall + ageNiall * 5;
+
+if (scoreJohn > scoreMark && scoreJohn > scoreNiall) {
+    console.log('John wins and his score is ' + scoreJohn);
+} else if (scoreNiall > scoreJohn && scoreNiall > scoreMark) {
+    console.log('Niall wins and his score is ' + scoreNiall);
+
+} else if (scoreJohn === scoreMark && scoreMark === scoreNiall) {
+    console.log('None wins. They have the same scores. Johns score is ' + scoreJohn + '. Mark score is ' + scoreMark + '. Niall score is ' + scoreNiall + '.');
+} else {
+    console.log('Mark wins and his score is ' + scoreMark);
 }
-else {
-    console.log('John is a man');
-}
 
-var job = 'student';
 
-job =  prompt('What does John do');
 
-switch (job) {
-    case 'student':
-        console.log('John studies JS');
-        break;
-    case 'driver':
-        console.log('John drives a cab in Lisbon');
-        break;
-    case 'cop':
-        console.log('John helps fight crime');
-        break;
-    default:
-        console.log('John does something else');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
