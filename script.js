dcfcdf09d7e1d57782c79070d699acd4ef3eb123
Naill <2jetsetter@gmail.com>
2017-06-10 12:27:44 +0300

    Loops

--- script.js
+++ script.js
@@ -278,7 +278,7 @@ var age = john.calculateAge();
 john.age = age;
 
 console.log(john);
- */
+
 
 var john  = {
     name: 'John',
@@ -308,6 +308,51 @@ var niall  = {
 
 niall.calculateAge();
 console.log(niall);
+ */
+
+
+///////////////////////////
+///// Loops
+
+// for (var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+
+//for loops
+// for (var i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+// for (var i = names.length - 1; i >= 0; i--){
+//     console.log(names[i]);
+// }
+
+
+//while loops
+// var i = 0;
+// while (i < names.length) {
+//     console.log(names[i]);
+//     i++;
+// }
+
+// for (var i = 1; i <= 5; i++){
+//     console.log(i);
+//
+//     if (i === 3){
+//         break;
+//     }
+// }
+
+for (var i = 1; i <= 5; i++){
+    if (i === 3){
+        continue;
+    }
+
+    console.log(i);
+}
 
 
 
