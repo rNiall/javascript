bf1ab9063470cf40290f49607af49876e9c69f69
Naill <2jetsetter@gmail.com>
2017-06-15 14:09:26 +0300

    objects and prototypes

---
+++ section_5/script.js
@@ -0,0 +1,61 @@
+// Function constructor
+/*
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function (name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person.prototype.calculateAge = function () {
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1987, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+console.log(Person.prototype);
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
+*/
+
+
+// Object.create
+
+var personProto = {
+    calculateAge: function () {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: {value: 'Jane'},
+    yearOfBirth: { value: 1969 },
+    job: {value: 'designer'}
+});
+
+
+
+
+
+
